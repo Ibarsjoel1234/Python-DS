afc22a27fe388f0b77aa63f17b34a297a281c7c8
Ibarsjoel1234 <35426719+Ibarsjoel1234@users.noreply.github.com>
2020-10-03 22:48:14 +0530

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -26,11 +26,11 @@ PROGRAM:
 
 def fizzBuzz(n):
  for i in range(1,n+1):
-  if n % 3 == 0 and n % 5 == 0:
+  if i % 3 == 0 and i % 5 == 0:
     return "FizzBuzz"
-  elif n % 3 == 0:
+  elif i % 3 == 0:
     return "Fizz"
-  elif n % 5 == 0:
+  elif i % 5 == 0:
     return "Buzz"
   else:
     return str(n)
