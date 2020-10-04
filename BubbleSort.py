3eeb2b9c3ccf9d80a310af077502c43d6a3bbefa
Ibarsjoel1234 <35426719+Ibarsjoel1234@users.noreply.github.com>
2020-10-04 10:48:05 +0530

    Create BubbleSort.py

---
+++ BubbleSort.py
@@ -0,0 +1,18 @@
+def bubblesort(a):
+  count = 0
+  size = len(a)
+  for i in range(size):
+    for j in range(i+1,size):
+      if a[i] > a[j]:
+        a[i],a[j] = a[j],a[i]
+        count = count + 1
+
+a = []
+s = int(input("Enter the size: "))
+print("Enter the Numbers: ")
+for f in range(0,s):
+  f = int(input())
+  a.append(f)
+print(f'Before Swapping: {a}')
+bubblesort(a)
+print(f'After  Swapping: {a}')
